d3654ba74588b35f72a9c8ba420147b0b7edadcb
Aaron S George <aaronsunilg@gmail.com>
2025-03-17 20:53:58 +0530

    admin gela

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -11,11 +11,11 @@ import {
 } from "../controllers/orderController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(addOrderItems).get(admin, getOrders);
+router.route("/").post(addOrderItems).get(getOrders);
 router.route("/mine").get(getMyOrders);
-router.route("/user/:id").get(admin, getOrdersByUserId);
+router.route("/user/:id").get(getOrdersByUserId);
 router.route("/:id").get(getOrderById);
 router.route("/:id/pay").put(updateOrderToPaid);
-router.route("/:id/deliver").put(admin, updateOrderToDelivered);
+router.route("/:id/deliver").put(updateOrderToDelivered);
 
 export default router;
