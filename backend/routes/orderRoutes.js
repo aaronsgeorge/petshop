9d23b60ed07bf45e665dcbd6e9530a76771a9df0
Aaron S George <aaronsunilg@gmail.com>
2025-03-17 20:45:48 +0530

    protect gela

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -11,11 +11,11 @@ import {
 } from "../controllers/orderController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route("/mine").get(protect, getMyOrders);
-router.route("/user/:id").get(protect, admin, getOrdersByUserId);
-router.route("/:id").get(protect, getOrderById);
-router.route("/:id/pay").put(protect, updateOrderToPaid);
-router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
+router.route("/").post(addOrderItems).get(admin, getOrders);
+router.route("/mine").get(getMyOrders);
+router.route("/user/:id").get(admin, getOrdersByUserId);
+router.route("/:id").get(getOrderById);
+router.route("/:id/pay").put(updateOrderToPaid);
+router.route("/:id/deliver").put(admin, updateOrderToDelivered);
 
 export default router;
