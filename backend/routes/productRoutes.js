b969fbcf2ec53bb0bed6662d11350f7ef6d94e27
Aaron S George <aaronsunilg@gmail.com>
2025-03-18 08:09:42 +0530

    admin,protect

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,11 +9,11 @@ import {
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/").get(getProducts).post(createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 export default router;
